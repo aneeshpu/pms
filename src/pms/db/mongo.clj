b6cd437429301e92197342c94009e0a7d1c509dd
Aneesh P U <aneeshpu@gmail.com>
2013-12-14 18:58:49 +0530

    Added a namespace for connection specific to mongodb. Moved connecting and inserting code into that namespace

---
+++ src/pms/db/mongo.clj
@@ -0,0 +1,13 @@
+(ns pms.db.mongo
+  (:require [monger.core :as monger]
+            [monger.collection :as monger-coll])
+  (:import [org.bson.types ObjectId]))
+
+(defn connect-db
+  []
+  (monger/connect!)
+  (monger/set-db! (monger/get-db "pms")))
+
+(defn insert
+  [documents & rest]
+  (monger-coll/insert documents (assoc (apply hash-map rest) :id (ObjectId.))))
--- src/pms/domain/patient.clj
+++ src/pms/domain/patient.clj
@@ -1,11 +1,9 @@
 (ns pms.domain.patient
-  (:require  [monger.core :as monger]
-             [monger.collection :as monger-coll])
-  (:import [org.bson.types ObjectId]))
+  (:require [pms.db.mongo :as pms-mongo]))
+
 
 (defn save
   [& {:keys [name age]}]
   (println "inside patient/save")
-  (monger/connect!)
-  (monger/set-db! (monger/get-db "pms"))
-  (monger-coll/insert "patients" {:id (ObjectId.) :name name :age age}))
+  (pms-mongo/connect-db)
+  (pms-mongo/insert "patients" :name name :age age))
